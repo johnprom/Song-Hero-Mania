8b065834d602c467cf815338ec9e467dcff3dc4b
John Prominski <jprom254@gmail.com>
2018-04-14 13:12:07 -0400

    added moving circle

--- Hero.py
+++ Hero.py
@@ -1,22 +1,28 @@
 import pygame, sys
 from pygame.locals import *
+
 pygame.init()
 
 screen = pygame.display.set_mode((1440, 900))
 
 circle = pygame.image.load("circle.png")
+circle = pygame.transform.scale(circle, (300, 300))
+circlerect = circle.get_rect()
+
 green = 0, 255, 0
 
+
 def update():
-    print("")
+    circlerect.x += 1
+
 
 def draw():
     screen.fill(green)
+    screen.blit(circle, circlerect)
     pygame.display.flip()
 
 
 if __name__ == "__main__":
-
 
     while True:
         for event in pygame.event.get():
