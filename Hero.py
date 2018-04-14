d8780676908056e810a9213e0ad24a32d4404128
John Prominski <jprom254@gmail.com>
2018-04-14 12:42:38 -0400

    added background and image

--- Hero.py
+++ Hero.py
@@ -2,17 +2,21 @@ import pygame, sys
 from pygame.locals import *
 pygame.init()
 
+screen = pygame.display.set_mode((1440, 900))
 
+circle = pygame.image.load("circle.png")
+green = 0, 255, 0
 
 def update():
     print("")
 
 def draw():
-    print("")
+    screen.fill(green)
+    pygame.display.flip()
 
 
 if __name__ == "__main__":
-    screen = pygame.display.set_mode((1440, 900))
+
 
     while True:
         for event in pygame.event.get():
