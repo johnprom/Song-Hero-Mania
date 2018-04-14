328081274c798c877996d4ee18e8e4bba99b78d6
John Prominski <jprom254@gmail.com>
2018-04-14 11:36:39 -0400

    setup environment

---
+++ Hero.py
@@ -0,0 +1,10 @@
+
+
+if __name__ == "__main__":
+    running = True
+    while running:
+        prompt = input("Continue ? (Y/N)")
+        if prompt[0] != 'Y' or prompt[0] != 'y':
+            running = False
+
+
