c8c9f7fd6519fb93a001813cf81ae877c46883ea
John Prominski <jprom254@gmail.com>
2018-04-14 12:28:48 -0400

    added ability to close program

--- Hero.py
+++ Hero.py
@@ -2,17 +2,25 @@ import pygame, sys
 from pygame.locals import *
 pygame.init()
 
+
+
 def update():
-    print("update")
+    print("")
 
 def draw():
-    print("draw")
+    print("")
 
 
 if __name__ == "__main__":
     screen = pygame.display.set_mode((1440, 900))
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN and event.key == ord("x"):
+                sys.exit()
+
         update()
         draw()
         pygame.time.delay(16)
