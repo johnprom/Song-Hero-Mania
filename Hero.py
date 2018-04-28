f051e3527d15e279f6c814ce5980956acd4be5ba
Unknown <jprom254@gmail.com>
2018-04-28 15:02:55 -0400

    made game playable

--- Hero.py
+++ Hero.py
@@ -3,6 +3,12 @@ from pygame.locals import *
 
 pygame.init()
 
+# global variables
+myfont = pygame.font.SysFont("monospace", 32)
+
+pygame.mixer.music.load("music.mp3")
+pygame.mixer.music.play(0,0.0)
+
 screen = pygame.display.set_mode((1440, 900))
 
 circle = pygame.image.load("circle.png")
@@ -17,14 +23,15 @@ notes = []
 tick_current = 0
 tick_rate = 60
 frame_rate = 60
+score = 0
 
-
+# classes
 class Note:
     tick = 0
     column = 0
     hit = False
 
-
+# functions
 def update():
     global tick_current
     tick_current += tick_rate/frame_rate
@@ -33,7 +40,11 @@ def update():
 def draw():
     screen.fill(green)
 
-    # temp
+    # draw text
+    label = myfont.render (str(score), 1, (0, 0, 0))
+    screen.blit(label, (100, 100))
+
+    # draws hit location circles
     for i in range(0, 4):
         circle_rect.x = 350 + 200 * i
         circle_rect.y = 600
@@ -71,6 +82,23 @@ def init_notes():
     notes.append(note4)
 
 
+def proccess_input(column):
+    for note in notes:
+        if not note.column == column:
+            continue
+        if note.hit == True:
+            continue
+        if abs(note.tick - tick_current)<=16:
+            note.hit = True
+            global score
+            score += score_calulator(note.tick)
+            print (score)
+
+
+def score_calulator(note_tick):
+    return 16-abs(note_tick - tick_current)
+
+
 if __name__ == "__main__":
 
     init_notes()
@@ -81,6 +109,14 @@ if __name__ == "__main__":
                 sys.exit()
             elif event.type == pygame.KEYDOWN and event.key == ord("x"):
                 sys.exit()
+            elif event.type == pygame.KEYDOWN and event.key == ord("d"):
+                proccess_input(0)
+            elif event.type == pygame.KEYDOWN and event.key == ord("f"):
+                proccess_input(1)
+            elif event.type == pygame.KEYDOWN and event.key == ord("j"):
+                proccess_input(2)
+            elif event.type == pygame.KEYDOWN and event.key == ord("k"):
+                proccess_input(3)
 
         update()
         draw()
